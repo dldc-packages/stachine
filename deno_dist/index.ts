dfab37d06c870d7026caa95a85566e0fedf5f043
Etienne Dldc <e.deladonchamps@gmail.com>
2020-11-20 18:22:19 +0100

    Remove miid and add tests

--- deno_dist/index.ts
+++ deno_dist/index.ts
@@ -1,31 +1,40 @@
 import { Subscription, SubscribeMethod } from 'https://raw.githubusercontent.com/etienne-dldc/suub/v3.1.1/deno_dist/mod.ts';
-import * as Miid from 'https://raw.githubusercontent.com/etienne-dldc/miid/v4.0.0-0/deno_dist/mod.ts';
+
+export const CANCEL_TOKEN = Symbol.for('STACHINE_CANCEL_TOKEN');
 
 export type Result<States extends UnionBase, Events extends UnionBase> =
-  | States
   | null
+  | typeof CANCEL_TOKEN
+  | States
   | StateWithEffect<States, Events>;
-export type Middleware<States extends UnionBase, Events extends UnionBase> = Miid.Middleware<
-  Result<States, Events>
->;
-export type Middlewares<States extends UnionBase, Events extends UnionBase> = Miid.Middlewares<
-  Result<States, Events>
->;
+
 export type UnionBase = { type: string };
+
 export type EffectCleanup = () => void;
+
 export type EmitEvents<Events extends UnionBase> = (event: Events) => void;
+
 export type InitialStateFn<States extends UnionBase, Events extends UnionBase> = (
   options: InitialTools<States, Events>
 ) => States | StateWithEffect<States, Events>;
-export type Handler<
+
+export type GlobalEffectFn<States extends UnionBase, Events extends UnionBase> = (
+  options: GlobalEffectTools<States, Events>
+) => EffectCleanup | void;
+
+export type TypedHandler<
   States extends UnionBase,
   Events extends UnionBase,
   S extends States['type'],
   E extends Events['type']
 > = (
   event: Extract<Events, { type: E }>,
-  state: Extract<States, { type: S }>,
-  next: () => Result<States, Events>
+  state: Extract<States, { type: S }>
+) => Result<States, Events>;
+
+export type Handler<States extends UnionBase, Events extends UnionBase> = (
+  event: Events,
+  state: States
 ) => Result<States, Events>;
 
 export type EffectTools<States extends UnionBase, Events extends UnionBase> = {
@@ -38,25 +47,43 @@ export type Effect<States extends UnionBase, Events extends UnionBase> = (
   tools: EffectTools<States, Events>
 ) => EffectCleanup | void;
 
+export type EventsObjectHandler<
+  States extends UnionBase,
+  Events extends UnionBase,
+  S extends States['type']
+> = {
+  [E in Events['type']]?: TypedHandler<States, Events, S, E>;
+};
+
+export type StatesObjectHandler<
+  States extends UnionBase,
+  Events extends UnionBase,
+  E extends Events['type']
+> = {
+  [S in States['type']]?: TypedHandler<States, Events, S, E>;
+};
+
 export type HandleObjectByStates<States extends UnionBase, Events extends UnionBase> = {
   [S in States['type']]?:
-    | {
-        [E in Events['type']]?: Handler<States, Events, S, E>;
-      }
-    | Handler<States, Events, S, Events['type']>;
+    | EventsObjectHandler<States, Events, S>
+    | TypedHandler<States, Events, S, Events['type']>;
 };
 
 export type HandleObjectByEvent<States extends UnionBase, Events extends UnionBase> = {
   [E in Events['type']]?:
-    | {
-        [S in States['type']]?: Handler<States, Events, S, E>;
-      }
-    | Handler<States, Events, States['type'], E>;
+    | StatesObjectHandler<States, Events, E>
+    | TypedHandler<States, Events, States['type'], E>;
 };
 
 export type StateMachineOptions<States extends UnionBase, Events extends UnionBase> = {
-  debug?: boolean;
   initialState: States | InitialStateFn<States, Events>;
+  globalEffect?: GlobalEffectFn<States, Events>;
+  debug?: boolean;
+};
+
+export type GlobalEffectTools<States extends UnionBase, Events extends UnionBase> = {
+  emit: EmitEvents<Events>;
+  getState: () => States;
 };
 
 export type InitialTools<States extends UnionBase, Events extends UnionBase> = {
@@ -65,65 +92,64 @@ export type InitialTools<States extends UnionBase, Events extends UnionBase> = {
 };
 
 export type BuilderTools<States extends UnionBase, Events extends UnionBase> = {
-  StateConsumer: Miid.ContextConsumer<States, true>;
-  EventConsumer: Miid.ContextConsumer<Events, true>;
-  compose: (...middlewares: Array<Middleware<States, Events>>) => Middleware<States, Events>;
+  withEffect(state: States, effect: Effect<States, Events>): StateWithEffect<States, Events>;
+  compose: (...handlers: Array<Handler<States, Events>>) => Handler<States, Events>;
   handleState<S extends States['type']>(
     state: S | ReadonlyArray<S>,
-    handler: Handler<States, Events, S, Events['type']>
-  ): Middleware<States, Events>;
+    handler:
+      | TypedHandler<States, Events, S, Events['type']>
+      | EventsObjectHandler<States, Events, S>
+  ): Handler<States, Events>;
   handleEvent<E extends Events['type']>(
     event: E | ReadonlyArray<E>,
-    handler: Handler<States, Events, States['type'], E>
-  ): Middleware<States, Events>;
+    handler:
+      | TypedHandler<States, Events, States['type'], E>
+      | StatesObjectHandler<States, Events, E>
+  ): Handler<States, Events>;
   handle<S extends States['type'], E extends Events['type']>(
     state: S | ReadonlyArray<S>,
     event: E | ReadonlyArray<E>,
-    handler: Handler<States, Events, S, E>
-  ): Middleware<States, Events>;
-  handleAny(
-    handler: Handler<States, Events, States['type'], Events['type']>
-  ): Middleware<States, Events>;
-  objectByStates(obj: HandleObjectByStates<States, Events>): Middleware<States, Events>;
-  objectByEvents(obj: HandleObjectByEvent<States, Events>): Middleware<States, Events>;
-  withEffect(state: States, effect: Effect<States, Events>): StateWithEffect<States, Events>;
+    handler: TypedHandler<States, Events, S, E>
+  ): Handler<States, Events>;
+  objectByStates(obj: HandleObjectByStates<States, Events>): Handler<States, Events>;
+  objectByEvents(obj: HandleObjectByEvent<States, Events>): Handler<States, Events>;
 };
 
 export type Builder<States extends UnionBase, Events extends UnionBase> = (
   tools: BuilderTools<States, Events>
-) => Middleware<States, Events>;
-
-const StateCtx = Miid.createContext<unknown>(null);
-const EventCtx = Miid.createContext<unknown>(null);
-
-export const StateConsumer = StateCtx.Consumer;
-export const EventConsumer = EventCtx.Consumer;
+) => Handler<States, Events>;
 
 export class StateMachine<States extends UnionBase, Events extends UnionBase> {
-  private readonly middleware: Middleware<States, Events>;
+  private readonly handler: Handler<States, Events>;
   private readonly subscription = Subscription<States>() as Subscription<States>;
   private readonly debug: boolean;
+  private globalCleanup: EffectCleanup | null = null;
   private currentCleanup: EffectCleanup | null = null;
   private currentState!: States;
   private destroyed = false;
 
   constructor(options: StateMachineOptions<States, Events>, builder: Builder<States, Events>) {
-    const { debug = false, initialState } = options;
-    this.middleware = builder({
-      StateConsumer: StateConsumer as Miid.ContextConsumer<States, true>,
-      EventConsumer: EventConsumer as Miid.ContextConsumer<Events, true>,
+    const { debug = false, initialState, globalEffect } = options;
+
+    this.handler = builder({
       compose,
       handle,
-      handleAny,
       handleEvent,
       handleState,
       withEffect,
       objectByEvents,
       objectByStates,
     });
-    this.debug = debug;
     const initialStateFn = typeof initialState === 'function' ? initialState : () => initialState;
     this.handleResult(initialStateFn({ withEffect, emit: this.emit }));
+
+    if (globalEffect) {
+      const cleanup = globalEffect({ emit: this.emit, getState: this.getState });
+      if (cleanup) {
+        this.globalCleanup = cleanup;
+      }
+    }
+    this.debug = debug;
   }
 
   getState = () => this.currentState;
@@ -133,21 +159,18 @@ export class StateMachine<States extends UnionBase, Events extends UnionBase> {
   emit = (event: Events) => {
     if (this.destroyed) {
       if (this.debug) {
-        console.warn(`Calling emit on an already destroyed machine. This is a no-op`);
+        console.warn(`[Stachine] Calling emit on an already destroyed machine is a no-op`);
       }
       return;
     }
 
-    const result = this.middleware(
-      Miid.ContextStack.createFrom(StateCtx.Provider(this.currentState), EventCtx.Provider(event)),
-      () => null
-    );
+    const result = this.handler(event, this.currentState);
 
     if (result === null) {
       // do nothing
       if (this.debug) {
         console.info(
-          `[StateMachine] Event ${event.type} on state ${this.currentState.type} has been canceled (transition returned null)`
+          `[Stachine] Event "${event.type}" on state "${this.currentState.type}" has been ignored (transition returned null or CANCEL_TOKEN)`
         );
       }
       return;
@@ -161,26 +184,34 @@ export class StateMachine<States extends UnionBase, Events extends UnionBase> {
   destroy() {
     if (this.destroyed) {
       if (this.debug) {
-        console.warn(`Calling destroy on an already destroyed machine`);
+        console.warn(`[Stachine] Calling destroy on an already destroyed machine is a no-op`);
       }
       return;
     }
     this.destroyed = true;
     this.cleanup();
     this.subscription.unsubscribeAll();
+    if (this.globalCleanup) {
+      this.globalCleanup();
+    }
   }
 
   private handleResult(result: Result<States, Events>): boolean {
-    if (result === null) {
+    if (result === CANCEL_TOKEN || result === null) {
       return false;
     }
-    this.cleanup();
     const nextState = result instanceof StateWithEffect ? result.state : result;
     const nextEffect = result instanceof StateWithEffect ? result.effect : null;
     const stateChanged = this.currentState !== nextState;
-    if (stateChanged) {
-      this.currentState = nextState;
+    if (stateChanged === false) {
+      if (nextEffect) {
+        this.cleanup();
+        this.runEffect(nextEffect);
+      }
+      return false;
     }
+    this.cleanup();
+    this.currentState = nextState;
     if (nextEffect) {
       this.runEffect(nextEffect);
     }
@@ -245,52 +276,56 @@ export function withEffect<States extends UnionBase, Events extends UnionBase>(
   return new StateWithEffect(state, effect);
 }
 
+export function compose<States extends UnionBase, Events extends UnionBase>(
+  ...handlers: Array<Handler<States, Events>>
+): Handler<States, Events> {
+  return (event, state) => {
+    for (const handler of handlers) {
+      const res = handler(event, state);
+      if (res) {
+        return res;
+      }
+    }
+    return null;
+  };
+}
+
 export function objectByStates<States extends UnionBase, Events extends UnionBase>(
   obj: HandleObjectByStates<States, Events>
-): Middleware<States, Events> {
-  return (ctx, next) => {
-    const state = ctx.getOrFail(StateConsumer as Miid.ContextConsumer<States, true>);
-    const event = ctx.getOrFail(EventConsumer as Miid.ContextConsumer<Events, true>);
+): Handler<States, Events> {
+  return (event, state) => {
     const stateHandler = (obj as any)[state.type];
     if (!stateHandler) {
-      return next(ctx);
+      return null;
     }
     if (typeof stateHandler === 'function') {
-      return stateHandler(event, state, () => next(ctx));
+      return stateHandler(event, state);
     }
     const handler = stateHandler[event.type];
     if (!handler) {
-      return next(ctx);
+      return null;
     }
-    return handler(event, state, () => next(ctx));
+    return handler(event, state);
   };
 }
 
 export function objectByEvents<States extends UnionBase, Events extends UnionBase>(
   obj: HandleObjectByEvent<States, Events>
-): Middleware<States, Events> {
-  return (ctx, next) => {
-    const state = ctx.getOrFail(StateConsumer as Miid.ContextConsumer<States, true>);
-    const event = ctx.getOrFail(EventConsumer as Miid.ContextConsumer<Events, true>);
+): Handler<States, Events> {
+  return (event, state) => {
     const eventHandler = (obj as any)[event.type];
     if (!eventHandler) {
-      return next(ctx);
+      return null;
     }
     if (typeof eventHandler === 'function') {
-      return eventHandler(event, state, () => next(ctx));
+      return eventHandler(event, state);
     }
     const handler = eventHandler[state.type];
     if (!handler) {
-      return next(ctx);
+      return null;
     }
-    return handler(event, state, () => next(ctx));
+    return handler(event, state);
   };
-}
-
-export function compose<States extends UnionBase, Events extends UnionBase>(
-  ...middlewares: Middlewares<States, Events>
-) {
-  return Miid.compose(...middlewares);
 }
 
 export function handleState<
@@ -299,16 +334,21 @@ export function handleState<
   S extends States['type']
 >(
   state: S | ReadonlyArray<S>,
-  handler: Handler<States, Events, S, Events['type']>
-): Middleware<States, Events> {
+  handler: TypedHandler<States, Events, S, Events['type']> | EventsObjectHandler<States, Events, S>
+): Handler<States, Events> {
   const stateArr = Array.isArray(state) ? state : [state];
-  return (ctx, next) => {
-    const state = ctx.getOrFail(StateConsumer as Miid.ContextConsumer<States, true>);
-    const event = ctx.getOrFail(EventConsumer as Miid.ContextConsumer<Events, true>);
+  return (event, state) => {
     if (!stateArr.includes(state.type)) {
-      return next(ctx);
+      return null;
     }
-    return handler(event as any, state as any, () => next(ctx));
+    if (typeof handler === 'function') {
+      return handler(event as any, state as any);
+    }
+    const handlerFn = (handler as any)[event.type];
+    if (!handlerFn) {
+      return null;
+    }
+    return handlerFn(event as any, state as any);
   };
 }
 
@@ -318,16 +358,21 @@ export function handleEvent<
   E extends Events['type']
 >(
   event: E | ReadonlyArray<E>,
-  handler: Handler<States, Events, States['type'], E>
-): Middleware<States, Events> {
+  handler: TypedHandler<States, Events, States['type'], E> | StatesObjectHandler<States, Events, E>
+): Handler<States, Events> {
   const eventArr = Array.isArray(event) ? event : [event];
-  return (ctx, next) => {
-    const state = ctx.getOrFail(StateConsumer as Miid.ContextConsumer<States, true>);
-    const event = ctx.getOrFail(EventConsumer as Miid.ContextConsumer<Events, true>);
+  return (event, state) => {
     if (!eventArr.includes(event.type)) {
-      return next(ctx);
+      return null;
     }
-    return handler(event as any, state as any, () => next(ctx));
+    if (typeof handler === 'function') {
+      return handler(event as any, state as any);
+    }
+    const handlerFn = (handler as any)[state.type];
+    if (!handlerFn) {
+      return null;
+    }
+    return handlerFn(event as any, state as any);
   };
 }
 
@@ -339,26 +384,14 @@ export function handle<
 >(
   state: S | ReadonlyArray<S>,
   event: E | ReadonlyArray<E>,
-  handler: Handler<States, Events, S, E>
-): Middleware<States, Events> {
+  handler: TypedHandler<States, Events, S, E>
+): Handler<States, Events> {
   const eventArr = Array.isArray(event) ? event : [event];
   const stateArr = Array.isArray(state) ? state : [state];
-  return (ctx, next) => {
-    const state = ctx.getOrFail(StateConsumer as Miid.ContextConsumer<States, true>);
-    const event = ctx.getOrFail(EventConsumer as Miid.ContextConsumer<Events, true>);
+  return (event, state) => {
     if (!eventArr.includes(event.type) || !stateArr.includes(state.type)) {
-      return next(ctx);
+      return null;
     }
-    return handler(event as any, state as any, () => next(ctx));
-  };
-}
-
-export function handleAny<States extends UnionBase, Events extends UnionBase>(
-  handler: Handler<States, Events, States['type'], Events['type']>
-): Middleware<States, Events> {
-  return (ctx, next) => {
-    const state = ctx.getOrFail(StateConsumer as Miid.ContextConsumer<States, true>);
-    const event = ctx.getOrFail(EventConsumer as Miid.ContextConsumer<Events, true>);
-    return handler(event as any, state as any, () => next(ctx));
+    return handler(event as any, state as any);
   };
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -10,19 +10,7 @@ test('create a state machine without arre', () => {
 });
 
 test('simple machine', () => {
-  type States = { type: 'Home' } | { type: 'Bed' } | { type: 'Work' };
-  type Events = { type: 'Commute' } | { type: 'Wake' } | { type: 'Sleep' };
-
-  const machine = new StateMachine<States, Events>(
-    { initialState: { type: 'Home' } },
-    ({ compose, handle }) =>
-      compose(
-        handle('Home', 'Commute', () => ({ type: 'Work' })),
-        handle('Home', 'Sleep', () => ({ type: 'Bed' })),
-        handle('Work', 'Commute', () => ({ type: 'Home' })),
-        handle('Bed', 'Wake', () => ({ type: 'Home' }))
-      )
-  );
+  const machine = createHomeMachine({ debug: false });
 
   expect(machine.getState()).toEqual({ type: 'Home' });
   machine.emit({ type: 'Wake' });
@@ -91,25 +79,7 @@ test('simple machine with initialState function', () => {
 });
 
 test('simple machine with object handler', () => {
-  type States = { type: 'Home' } | { type: 'Bed' } | { type: 'Work' };
-  type Events = { type: 'Commute' } | { type: 'Wake' } | { type: 'Sleep' };
-
-  const machine = new StateMachine<States, Events>(
-    { initialState: { type: 'Home' } },
-    ({ objectByStates }) =>
-      objectByStates({
-        Home: {
-          Commute: () => ({ type: 'Work' }),
-          Sleep: () => ({ type: 'Bed' }),
-        },
-        Work: {
-          Commute: () => ({ type: 'Home' }),
-        },
-        Bed: {
-          Wake: () => ({ type: 'Home' }),
-        },
-      })
-  );
+  const machine = createHomeMachine({ debug: false });
 
   expect(machine.getState()).toEqual({ type: 'Home' });
   machine.emit({ type: 'Wake' });
@@ -125,25 +95,7 @@ test('simple machine with object handler', () => {
 });
 
 test('simple machine with object handler', () => {
-  type States = { type: 'Home' } | { type: 'Bed' } | { type: 'Work' };
-  type Events = { type: 'Commute' } | { type: 'Wake' } | { type: 'Sleep' };
-
-  const machine = new StateMachine<States, Events>(
-    { initialState: { type: 'Home' } },
-    ({ objectByEvents }) =>
-      objectByEvents({
-        Commute: {
-          Home: () => ({ type: 'Work' }),
-          Work: () => ({ type: 'Home' }),
-        },
-        Sleep: {
-          Home: () => ({ type: 'Bed' }),
-        },
-        Wake: {
-          Bed: () => ({ type: 'Home' }),
-        },
-      })
-  );
+  const machine = createHomeMachine({ debug: false });
 
   expect(machine.getState()).toEqual({ type: 'Home' });
   machine.emit({ type: 'Wake' });
@@ -157,3 +109,169 @@ test('simple machine with object handler', () => {
   machine.emit({ type: 'Sleep' });
   expect(machine.getState()).toEqual({ type: 'Bed' });
 });
+
+test('emit on destroyed machine should warn if debug', () => {
+  const consoleWarnSpy = jest.spyOn(global.console, 'warn');
+  consoleWarnSpy.mockImplementation(() => {});
+
+  const machine = createHomeMachine({ debug: true });
+
+  expect(machine.getState()).toEqual({ type: 'Home' });
+  machine.emit({ type: 'Commute' });
+  expect(machine.getState()).toEqual({ type: 'Work' });
+  machine.destroy();
+  machine.emit({ type: 'Commute' });
+  expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+  expect(consoleWarnSpy).toHaveBeenCalledWith(
+    '[Stachine] Calling emit on an already destroyed machine is a no-op'
+  );
+
+  consoleWarnSpy.mockRestore();
+});
+
+test('emit on destroyed machine should not warn if debug is false', () => {
+  const consoleWarnSpy = jest.spyOn(global.console, 'warn');
+  consoleWarnSpy.mockImplementation(() => {});
+
+  const machine = createHomeMachine({ debug: false });
+
+  expect(machine.getState()).toEqual({ type: 'Home' });
+  machine.emit({ type: 'Commute' });
+  expect(machine.getState()).toEqual({ type: 'Work' });
+  machine.destroy();
+  machine.emit({ type: 'Commute' });
+  expect(consoleWarnSpy).not.toHaveBeenCalled();
+
+  consoleWarnSpy.mockRestore();
+});
+
+test('global effect is run', () => {
+  const cleanup = jest.fn();
+  const effect = jest.fn(() => cleanup);
+
+  const machine = createBooleanMachine({ debug: false, globalEffect: effect });
+
+  expect(effect).toHaveBeenCalled();
+  expect(cleanup).not.toHaveBeenCalled();
+  expect(machine.getState()).toEqual({ type: 'Off' });
+  machine.emit({ type: 'Toggle' });
+  expect(machine.getState()).toEqual({ type: 'On' });
+  machine.destroy();
+  expect(cleanup).toHaveBeenCalled();
+});
+
+test('global effect no cleanup', () => {
+  const effect = jest.fn();
+
+  const machine = createBooleanMachine({ debug: false, globalEffect: effect });
+
+  expect(effect).toHaveBeenCalled();
+  expect(machine.getState()).toEqual({ type: 'Off' });
+  machine.emit({ type: 'Toggle' });
+  expect(machine.getState()).toEqual({ type: 'On' });
+  machine.destroy();
+});
+
+test('unhandled transitions should info if debug', () => {
+  const consoleInfoSpy = jest.spyOn(global.console, 'info');
+  consoleInfoSpy.mockImplementation(() => {});
+
+  const machine = createBooleanMachine({ debug: true });
+
+  expect(machine.getState()).toEqual({ type: 'Off' });
+  machine.emit({ type: 'TurnOff' });
+  expect(consoleInfoSpy).toHaveBeenCalledWith(
+    '[Stachine] Event "TurnOff" on state "Off" has been ignored (transition returned null or CANCEL_TOKEN)'
+  );
+
+  consoleInfoSpy.mockRestore();
+});
+
+test('returning previous state should not call state listener', () => {
+  type States = { type: 'On' } | { type: 'Off' };
+  type Events = { type: 'TurnOn' } | { type: 'TurnOff' } | { type: 'Toggle' } | { type: 'Noop' };
+
+  const machine = new StateMachine<States, Events>(
+    { initialState: { type: 'Off' } },
+    ({ objectByEvents }) =>
+      objectByEvents({
+        Noop: { Off: (_e, state) => state, On: (_e, state) => state },
+        Toggle: { Off: () => ({ type: 'On' }), On: () => ({ type: 'Off' }) },
+        TurnOn: { Off: () => ({ type: 'On' }) },
+        TurnOff: { On: () => ({ type: 'Off' }) },
+      })
+  );
+
+  const onStateChange = jest.fn();
+
+  machine.subscribe(onStateChange);
+  expect(machine.getState()).toEqual({ type: 'Off' });
+  machine.emit({ type: 'Toggle' });
+  expect(onStateChange).toHaveBeenCalledWith({ type: 'On' });
+  onStateChange.mockClear();
+  machine.emit({ type: 'Noop' });
+  expect(onStateChange).not.toHaveBeenCalled();
+});
+
+test('destroy twice does nothing', () => {
+  const machine = createBooleanMachine({ debug: false });
+
+  expect(machine.getState()).toEqual({ type: 'Off' });
+  machine.emit({ type: 'Toggle' });
+  expect(machine.getState()).toEqual({ type: 'On' });
+  machine.destroy();
+  expect(() => machine.destroy()).not.toThrow();
+});
+
+test('destroy twice warn if debug', () => {
+  const consoleWarnSpy = jest.spyOn(global.console, 'warn');
+  consoleWarnSpy.mockImplementation(() => {});
+
+  const machine = createBooleanMachine({ debug: true });
+
+  expect(machine.getState()).toEqual({ type: 'Off' });
+  machine.emit({ type: 'Toggle' });
+  expect(machine.getState()).toEqual({ type: 'On' });
+  machine.destroy();
+  expect(consoleWarnSpy).not.toHaveBeenCalled();
+  machine.destroy();
+  expect(consoleWarnSpy).toHaveBeenCalledWith(
+    '[Stachine] Calling destroy on an already destroyed machine is a no-op'
+  );
+
+  consoleWarnSpy.mockRestore();
+});
+
+function createBooleanMachine({ debug, globalEffect }: { debug: boolean; globalEffect?: any }) {
+  type States = { type: 'On' } | { type: 'Off' };
+  type Events = { type: 'TurnOn' } | { type: 'TurnOff' } | { type: 'Toggle' };
+
+  const machine = new StateMachine<States, Events>(
+    { initialState: { type: 'Off' }, debug, globalEffect },
+    ({ objectByEvents }) =>
+      objectByEvents({
+        Toggle: { Off: () => ({ type: 'On' }), On: () => ({ type: 'Off' }) },
+        TurnOn: { Off: () => ({ type: 'On' }) },
+        TurnOff: { On: () => ({ type: 'Off' }) },
+      })
+  );
+
+  return machine;
+}
+
+function createHomeMachine({ debug }: { debug: boolean }) {
+  type States = { type: 'Home' } | { type: 'Bed' } | { type: 'Work' };
+  type Events = { type: 'Commute' } | { type: 'Wake' } | { type: 'Sleep' };
+
+  const machine = new StateMachine<States, Events>(
+    { initialState: { type: 'Home' }, debug },
+    ({ objectByStates }) =>
+      objectByStates({
+        Home: { Commute: () => ({ type: 'Work' }), Sleep: () => ({ type: 'Bed' }) },
+        Work: { Commute: () => ({ type: 'Home' }) },
+        Bed: { Wake: () => ({ type: 'Home' }) },
+      })
+  );
+
+  return machine;
+}
